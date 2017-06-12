fb6545633eec8632df120587f6684152d26e2687
Ryan Leonard <rhinoisnot@gmail.com>
2017-06-12 13:50:11 -0400

    Updated tests to have current space replacement.

--- test/ExternalStatus/shieldEscape.js
+++ test/ExternalStatus/shieldEscape.js
@@ -2,5 +2,5 @@ const test = require("ava");
 const ExternalStatus = require("../../ExternalStatus");
 
 test("escapes a standard string", t => {
-  t.is(ExternalStatus.shieldEscape("Lint_1: 1-2-3"), "Lint__1:_1--2--3");
+  t.is(ExternalStatus.shieldEscape("Lint_1: 1-2-3"), "Lint__1:%201--2--3");
 });
