3bb4ec3cf68d4dae17bfe844f3146099711c90e2
Ryan Leonard <rhinoisnot@gmail.com>
2017-06-12 12:39:37 -0400

    Parameterized test.

--- test/ExternalStatus/status/token-header.js
+++ test/ExternalStatus/status/token-header.js
@@ -3,19 +3,21 @@ const nock = require("nock");
 const random = require("lodash.random");
 const ExternalStatus = require("../../../ExternalStatus");
 
+const token = "testing";
+
 test("passes 'token' parameter as a header", t => {
   const origin = `https://domain${random(1000, 10000)}.com`;
   const status = new ExternalStatus();
-  let token;
+  let tokenHeader;
   const scope = nock(origin)
-    .matchHeader("PRIVATE-TOKEN", (t) => { token = t; return true; })
+    .matchHeader("PRIVATE-TOKEN", (t) => { tokenHeader = t; return true; })
     .filteringPath(() => "/")
     .post("/", () => true)
     .reply(200);
   return status
-    .status({ gitlab: origin, token: "testing" })
+    .status({ gitlab: origin, token })
     .then(() => {
       t.true(scope.isDone());
-      t.is(token, "testing");
+      t.is(tokenHeader, token);
     });
 });
